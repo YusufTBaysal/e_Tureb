eca247cd38c26dd4c10c21f9d5c3a7a3647493fe
YusufTBaysal <yt_baysal06@hotmail.com>
2023-12-11 16:23:17 +0300

    a

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,55 +1,30 @@
 package com.example.test;
 
 import android.content.Intent;
+
 import androidx.annotation.NonNull;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import android.view.MenuItem;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity
-        implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_kategoriler);
 
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemReselectedListener(this);
 
     }
 
-    public boolean onNavigationItemSelected(@NonNull MenuItem item){
-
-        Fragment fragment = null;
-
-        switch (item.getItemId()) {
-            case R.id.home:
-                fragment = new HomeFragment();
-                break;
-            case R.id.chatgpt:
-                fragment = new ChatGptFragment();
-                break;
-            case R.id.maps:
-                fragment = new MapsFragment();
-                break;
-        }
-
-        return loadFragment(fragment);
-    }
-
-    private boolean loadFragment(Fragment fragment){
-        if(fragment != null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-            return true;
-        }
-        return false;
-    }
-
-    public void git1(View view){
+    public void git1(View view) {
         Intent intent = new Intent(this, Konaklama.class);
         startActivity(intent);
     }
@@ -63,5 +38,4 @@ public class MainActivity extends AppCompatActivity
         Intent intent = new Intent(this, Gezi.class);
         startActivity(intent);
     }
-
 }
